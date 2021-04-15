ce74d8703e6c39d91c91d5961d5e4e43c97bc970
Luka Mihelac <luka.mihelac7@gmail.com>
2021-04-15 11:01:48 +0200

    Project is all functional and finished

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const a = 2;
-console.log(a);
+const colors = ["red", "white", "blue", "grey", "orange"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    console.log(randomNumber);
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
